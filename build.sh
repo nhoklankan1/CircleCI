038f139d7b748b8486b74952f2de07cbbd785d06
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 19:24:47 +0700

    update

--- build.sh
+++ build.sh
@@ -5,15 +5,6 @@ BUILD_DIR=$BASE_DIR/DIO_Abstraction/Debug
 echo $BASE_DIR
 echo $BUILD_DIR
 
-echo "install toolchains"
-mkdir toolchains
-cd toolchains
-wget "https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz"
-ls -l
-tar -xf arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
-rm arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
-ls -l
-export PATH="~/toolchains/arm-gnu-toolchain-13.2.rel1/bin:$PATH"
 arm-none-eabi-gcc --version
 
 echo "build stm32"
