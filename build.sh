c42bbf5b47de7b6261e3542d056431a998178219
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 11:18:50 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ cd toolchains
 wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
 ls -l
 tar -jxf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+ls -l
