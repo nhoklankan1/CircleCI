f4a2777a87896abb15ff46a3f246cf219a6987d4
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 10:57:46 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ BASE_DIR=$PWD
 BUILD_DIR=$BASE_DIR/DIO_Abstraction/Debug
 echo $BASE_DIR
 echo $BUILD_DIR
-cd $BUILD_DIR
-ls -l
-make clean
+echo "install toolchains"
+mkdir toolchains
+cd /toolchains
+wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B
