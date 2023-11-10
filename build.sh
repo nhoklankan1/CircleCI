4fa98e4818ce5c9b2713a0050f5f4c2e25902ff7
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 10:33:34 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "Running Build and Save Script"
 BASE_DIR=$PWD
 ls -l
-BUILD_DIR=$BASE/DIO_Abstraction/Debug
+BUILD_DIR=$BASE_DIR/DIO_Abstraction/Debug
 echo $BASE_DIR
 echo $BUILD_DIR
 cd $BUILD_DIR
