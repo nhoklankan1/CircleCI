ae576a803ded4aced31240b6329ef61f5e9ce8af
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 11:01:45 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-apt-get install wget
+apt-get install --yes wget
 echo "Running Build and Save Script"
 BASE_DIR=$PWD
 BUILD_DIR=$BASE_DIR/DIO_Abstraction/Debug
@@ -9,3 +9,4 @@ echo "install toolchains"
 mkdir toolchains
 cd toolchains
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B
+ls -l
