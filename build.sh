0e744d4366c102d3339503544093e1a202e0d04d
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-10 11:21:31 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ cd toolchains
 wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
 ls -l
 tar -jxf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+rm gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 ls -l
+export PATH="~/CircleCI/toolchains/gcc-arm-none-eabi-10.3-2021.10/bin:$PATH"
+arm-none-eabi-gcc --version
