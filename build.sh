5784d2f3de6dc3a6a21b18ca469146fe1611a705
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-11 10:30:35 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 echo "Running Build and Save Script"
 BASE_DIR=$PWD
-BUILD_DIR=$BASE_DIR/DIO_Abstraction
+BUILD_DIR=$BASE_DIR/Build-STM32-With-CI-master
 echo $BASE_DIR
 echo $BUILD_DIR
 
