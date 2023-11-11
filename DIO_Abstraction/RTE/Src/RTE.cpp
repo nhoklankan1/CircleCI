263407856853c3a11216b5588143231a818dee73
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-11 10:00:44 +0700

    update RTE

--- DIO_Abstraction/RTE/Src/RTE.cpp
+++ DIO_Abstraction/RTE/Src/RTE.cpp
@@ -1,59 +1,59 @@
-#include "RTE.h"
+// #include "RTE.h"
 
-/**
- * @brief Pointer to the state of LED1.
- */
-GPIO_PinState *RTE_LED1;
+// /**
+//  * @brief Pointer to the state of LED1.
+//  */
+// GPIO_PinState *RTE_LED1;
 
-/**
- * @brief Pointer to the value of BUTTON1.
- */
-uint8_t *RTE_BUTTON1;
+// /**
+//  * @brief Pointer to the value of BUTTON1.
+//  */
+// uint8_t *RTE_BUTTON1;
 
-GPIO_PinState *RTE_STEPPER;
+// GPIO_PinState *RTE_STEPPER;
 
-/**
- * @brief Initialize the hardware configuration.
- */
-void RTE_HW_Init() {
-    ECU_Config();
-}
+// /**
+//  * @brief Initialize the hardware configuration.
+//  */
+// void RTE_HW_Init() {
+//     ECU_Config();
+// }
 
-/**
- * @brief Toggle the state of the LED.
- */
-void RTE_LED_Toggle() {
-    DIO_TogglePin(GPIOD, GPIO_PIN_12);
-}
+// /**
+//  * @brief Toggle the state of the LED.
+//  */
+// void RTE_LED_Toggle() {
+//     DIO_TogglePin(GPIOD, GPIO_PIN_12);
+// }
 
-/**
- * @brief Write a value to a pin.
- *
- * @param value The value to be written (0 or non-zero).
- */
-void RTE_Write(uint8_t value) {
-    GPIO_PinState temp;
-    RTE_STEPPER = &temp;
+// /**
+//  * @brief Write a value to a pin.
+//  *
+//  * @param value The value to be written (0 or non-zero).
+//  */
+// void RTE_Write(uint8_t value) {
+//     GPIO_PinState temp;
+//     RTE_STEPPER = &temp;
 
-    if (value == 0) {
-        temp = GPIO_PIN_RESET;
-    } else {
-        temp = GPIO_PIN_SET;
-    }
+//     if (value == 0) {
+//         temp = GPIO_PIN_RESET;
+//     } else {
+//         temp = GPIO_PIN_SET;
+//     }
 
-    DIO_WritePin(GPIOA, GPIO_PIN_1, *RTE_STEPPER);
-}
+//     DIO_WritePin(GPIOA, GPIO_PIN_1, *RTE_STEPPER);
+// }
 
-/**
- * @brief Read the value of a pin.
- *
- * @return The value read from the pin.
- */
-uint8_t RTE_Read() {
-    uint8_t temp;
-    RTE_BUTTON1 = &temp;
+// /**
+//  * @brief Read the value of a pin.
+//  *
+//  * @return The value read from the pin.
+//  */
+// uint8_t RTE_Read() {
+//     uint8_t temp;
+//     RTE_BUTTON1 = &temp;
 
-    temp = DIO_ReadPin(GPIOC, GPIO_PIN_9);
+//     temp = DIO_ReadPin(GPIOC, GPIO_PIN_9);
 
-    return *RTE_BUTTON1;
-}
+//     return *RTE_BUTTON1;
+// }
