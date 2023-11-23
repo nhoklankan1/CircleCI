a2a3d6c53f3c376366e67108672f0682e15bcb70
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-23 21:31:04 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ echo "==== Build Test Firmware for STM32 ===="
 
 echo "==== Version of GCC Toolchains ===="
 apt install -y cmake
+apt update
+apt install -y cmake build-essential
 arm-none-eabi-gcc --version
 cd $BUILD_DIR
 cmake -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_SOURCE_DIR}/gcc-arm-none-eabi.cmake
