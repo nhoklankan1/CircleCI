ddd24e1a9a319f288005e1059dcec0806dc49921
nhoklankan1 <69079702+nhoklankan1@users.noreply.github.com>
2023-11-23 21:00:59 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #/bin/bash
+BUILD_DIR=$BASE_DIR/DIO_Abstraction
 echo "==== Build Test Firmware for STM32 ===="
 
 echo "==== Version of GCC Toolchains ===="
 arm-none-eabi-gcc --version
 
 echo "==== Run Build ===="
+cd $BUILD_DIR
 make
